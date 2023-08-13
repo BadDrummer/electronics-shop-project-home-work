a4f074e8be3a96e0358697da0f4bb4bd3a04f31c
Максим <mr.maks31@mail.ru>
2023-08-14 02:52:03 +0300

    created test_apply_discount() in test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -8,3 +8,9 @@ item2 = Item("Ноутбук", 20000, 5)
 def test_calculate_total_price():
     assert item1.calculate_total_price() == 200000
     assert item2.calculate_total_price() == 100000
+
+
+def test_apply_discount():
+    Item.pay_rate = 0.8
+    assert item1.apply_discount() == 8000
+    assert item2.apply_discount() == 16000
