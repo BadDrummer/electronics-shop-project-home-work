ec6b5af957e7ad9787f5ea688d8b28132650d104
Максим <mr.maks31@mail.ru>
2023-08-20 23:33:28 +0300

    added test function in test_item.py for instantiate_from_csv() method  of item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -14,3 +14,8 @@ def test_apply_discount():
     Item.pay_rate = 0.8
     assert item1.apply_discount() == 8000
     assert item2.apply_discount() == 16000
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv()  # создание объектов из данных файла
+    assert len(Item.all) == 5
