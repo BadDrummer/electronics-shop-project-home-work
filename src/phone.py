d26f37df84d746039d9ae5deeb7ba90ec960e420
Максим <mr.maks31@mail.ru>
2023-08-27 20:30:21 +0300

    1) added getter and setter for number_of_sim class method 2) added __repr__ 3) made attribute number_of_sim as a protected attribute in class Phone in phone.py

--- src/phone.py
+++ src/phone.py
@@ -5,5 +5,18 @@ class Phone(Item):
 
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
         super().__init__(name, price, quantity)
-        self.number_of_sim = number_of_sim
+        self._number_of_sim = number_of_sim
 
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self._name}', {self.price}, {self.quantity}, {self._number_of_sim})"
+
+    @property
+    def number_of_sim(self):
+        return self._number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, value):
+        if isinstance(value, int) and value > 0:
+            self._number_of_sim = value
+        else:
+            raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля.")
